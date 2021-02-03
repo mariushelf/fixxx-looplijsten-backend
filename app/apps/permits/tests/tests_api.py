0a252bf5769a9865dd040a2dd3fab4ef88f8494f
Maurice Guikema <maurice@mgui.nl>
2021-02-03 13:46:14 +0100

    vergunningen: extra tests, fix serializer

--- app/apps/permits/tests/tests_api.py
+++ app/apps/permits/tests/tests_api.py
@@ -20,18 +20,61 @@ from app.utils.unittest_helpers import (
 
 class PermitViewSetTest(APITestCase):
     """
-    Tests for the API endpoints for retrieving case data
+    Tests for the API endpoints for retrieving permit data
     """
+
+    MOCK_BAG_ID = "0363010000809805"
+    PERMIT_URL_NAME = "all-permits-permit checkmarks"
+
+    def _get_url(self):
+        return "%s?bag_id=%s" % (
+            reverse(self.PERMIT_URL_NAME),
+            self.MOCK_BAG_ID,
+        )
 
     def test_unauthenticated_request(self):
         """
         An unauthenticated request should not be possible
         """
 
-        url = "%s?bag_id%s" % (
-            reverse("all-permits-permit checkmarks"),
-            "0363010000809805",
-        )
+        url = self._get_url()
         client = get_unauthenticated_client()
         response = client.get(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_authenticated_requests_no_bag_id(self):
+        """
+        An authenticated request fails if the requested id's doesn't have a bag_id
+        """
+
+        url = reverse(self.PERMIT_URL_NAME)
+        client = get_authenticated_client()
+        response = client.get(url)
+
+        # The response returns a 404
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    @patch("apps.permits.views.DecosJoinRequest.get_checkmarks_by_bag_id")
+    def test_authenticated_requests_succeeds(self, mock_get_checkmarks_by_bag_id):
+        """
+        An authenticated request succeeds and contains all the necessary data
+        """
+
+        MOCK_RESPONSE = {
+            "has_b_and_b_permit": "UNKNOWN",
+            "has_vacation_rental_permit": "UNKNOWN",
+            "has_splitsing_permit": "UNKNOWN",
+            "has_ontrekking_vorming_samenvoeging_permit": "UNKNOWN",
+            "has_omzettings_permit": "UNKNOWN",
+            "has_ligplaats_permit": "UNKNOWN",
+        }
+        mock_get_checkmarks_by_bag_id.return_value = MOCK_RESPONSE
+
+        url = self._get_url()
+        client = get_authenticated_client()
+        response = client.get(url)
+
+        # The response returns a 200
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEquals(response.json(), MOCK_RESPONSE)
